369b0184dbc1b97e53aa8da53ab80239023441a5
Keunoh <rmsdhcjswo1@gmail.com>
2022-11-08 17:05:21 +0900

    feature: request & response util

---
+++ src/main/frontend/src/utils/APIUtils.js
@@ -0,0 +1,23 @@
+const request = async (options) => {
+  const headers = new Headers({
+    "Content-Type": "application/json",
+  });
+
+  const defaults = { headers: headers };
+  options = Object.assign({}, defaults, options);
+
+  const response = await fetch(options.url, options);
+  const json = await response.json();
+  if (!response.ok) {
+    return Promise.reject(json);
+  }
+  return json;
+};
+
+export function join(joinMemberData) {
+  return request({
+    url: "http://localhost:8989/members/form",
+    method: "POST",
+    body: JSON.stringify(joinMemberData),
+  });
+}
