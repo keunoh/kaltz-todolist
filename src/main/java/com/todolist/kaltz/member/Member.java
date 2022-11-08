01707ff557048747ec92b6cfec7d44354828d094
Keunoh <rmsdhcjswo1@gmail.com>
2022-11-08 13:50:50 +0900

    feature: member entity

--- src/main/java/com/todolist/kaltz/member/Member.java
+++ src/main/java/com/todolist/kaltz/member/Member.java
@@ -1,29 +1,18 @@
 package com.todolist.kaltz.member;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 @Getter
-@NoArgsConstructor
-@Entity
-public class User {
+@Setter
+@RequiredArgsConstructor
+public class Member {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(nullable = false)
     private String name;
-
-    @Column(nullable = false)
     private String password;
 
-    @Builder
-    public User(String name, String password) {
-        this.name = name;
-        this.password = password;
-    }
+
 }
