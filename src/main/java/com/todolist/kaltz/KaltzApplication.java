a21320c2b0d88e995dbd818cf7d1f1f19660b375
Keunoh <rmsdhcjswo1@gmail.com>
2022-11-08 12:43:01 +0900

    init: project start

---
+++ src/main/java/com/todolist/kaltz/KaltzApplication.java
@@ -0,0 +1,13 @@
+package com.todolist.kaltz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaltzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaltzApplication.class, args);
+	}
+
+}
