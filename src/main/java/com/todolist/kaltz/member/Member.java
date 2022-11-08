4a4fc555f0f879993b6448f65a37a6fd7ed5e1cb
Keunoh <rmsdhcjswo1@gmail.com>
2022-11-08 12:52:46 +0900

    feature: member entity

---
+++ src/main/java/com/todolist/kaltz/member/Member.java
@@ -0,0 +1,13 @@
+package com.todolist.kaltz.member;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class Member {
+
+    private Long id;
+    private String name;
+    private String password;
+}
