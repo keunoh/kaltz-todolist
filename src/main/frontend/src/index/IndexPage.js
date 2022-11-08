1149a10e6b7746d740e05b45e7e0b3d5912f633d
Keunoh <rmsdhcjswo1@gmail.com>
2022-11-08 17:05:59 +0900

    update: add: login form

--- src/main/frontend/src/index/IndexPage.js
+++ src/main/frontend/src/index/IndexPage.js
@@ -1,7 +1,58 @@
+import "./IndexPage.css";
+import { useState } from "react";
+import { join } from "../utils/APIUtils";
+
 const IndexPage = () => {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const joinHandleSubmit = (e) => {
+    e.preventDefault();
+
+    let joinMemberData = {
+      name: name,
+      password: password,
+    };
+
+    join(joinMemberData)
+      .then((response) => {
+        console.log("Join Success");
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log("Join Fail");
+        console.log(error);
+      });
+  };
+
   return (
     <div>
       <h1>hello! from React!</h1>
+
+      <form onSubmit={joinHandleSubmit}>
+        <label htmlFor="name">ID : </label>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+
+        <label htmlFor="password">PW : </label>
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button>submit</button>
+      </form>
+
+      <div className="mydiv d1"></div>
+      <div className="mydiv d2"></div>
+      <div className="mydiv d3"></div>
     </div>
   );
 };
