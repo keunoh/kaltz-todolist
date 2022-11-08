b7f372591d6e8a3c73fda0572377669dbb736462
Keunoh <rmsdhcjswo1@gmail.com>
2022-11-08 14:51:52 +0900

    feature: member join controller

---
+++ src/main/java/com/todolist/kaltz/member/servlet/MemberData.java
@@ -0,0 +1,10 @@
+package com.todolist.kaltz.member.servlet;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class MemberData {
+    private String name;
+    private String password;
+}
